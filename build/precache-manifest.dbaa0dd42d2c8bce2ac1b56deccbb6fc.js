e39e59755df5060ce4af5bdb1f526f0259e25809
Wageesh Arya <slowlearningcalligraphy@gmail.com>
2020-05-20 18:51:24 +0530

    First commit

---
+++ build/precache-manifest.dbaa0dd42d2c8bce2ac1b56deccbb6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18857540c9e45e80e1b1d87cb1104c13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf797a8d45b248f963f",
+    "url": "/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "a6b4ce331188cf802306",
+    "url": "/static/js/2.c130e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c130e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf797a8d45b248f963f",
+    "url": "/static/js/main.365bf83f.chunk.js"
+  },
+  {
+    "revision": "637826cf51459acd408c",
+    "url": "/static/js/runtime-main.df367447.js"
+  }
+]);
